feebf8c7cf213e3b5c01f16e3b2e96c6054fcb40
Aoniq <89135@glr.nl>
2023-08-06 16:21:04 +0200

    Added fetchcontacts on message receive

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -18,6 +18,27 @@
         document.getElementById('message-input').value = '';
     });
 
+    function fetchContacts() {
+        fetch('/api/contacts')
+          .then(response => response.json())
+          .then(data => {
+            const contactsList = document.getElementById('contacts-list');
+    
+            // Clear the current contacts list
+            contactsList.innerHTML = '';
+    
+            // Loop through the contacts and create list items for each contact
+            data.forEach(contact => {
+              const listItem = document.createElement('li');
+              listItem.innerHTML = `<a href="/chat/${contact.user_id}">${contact.username}</a>`;
+              contactsList.appendChild(listItem);
+            });
+          })
+          .catch(error => {
+            console.error('Error fetching contacts:', error);
+          });
+      }
+
     // Receive message from server
     socket.on('chat message', function (msg) {
         let messageContainer = document.getElementById('chat-container');
@@ -34,29 +55,8 @@
         messageElement.innerHTML = `<strong>${msg.senderName}</strong> ${msg.message}`;
         messageContainer.appendChild(messageElement);
         scrollToBottom();
+        fetchContacts();
         });
-
-        function fetchContacts() {
-            fetch('/api/contacts')
-              .then(response => response.json())
-              .then(data => {
-                const contactsList = document.getElementById('contacts-list');
-        
-                // Clear the current contacts list
-                contactsList.innerHTML = '';
-        
-                // Loop through the contacts and create list items for each contact
-                data.forEach(contact => {
-                  const listItem = document.createElement('li');
-                  listItem.innerHTML = `<a href="/chat/${contact.user_id}">${contact.username}</a>`;
-                  contactsList.appendChild(listItem);
-                });
-              })
-              .catch(error => {
-                console.error('Error fetching contacts:', error);
-              });
-          }
-    // Tell the server to create a private room for this sender-receiver pair
 
     window.onload = function () {
         fetchContacts();
