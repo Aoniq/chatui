f6e3ed665beac23567ea1d3d2705f129acada0b0
Aoniq <89135@glr.nl>
2023-08-20 13:16:10 +0200

    Adding conversation lists

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -5,12 +5,23 @@
             const contactsList = document.getElementById('contacts-list');
     
             // Clear the current contacts list
-            contactsList.innerHTML = '';
+            // contactsList.innerHTML = '';
     
             // Loop through the contacts and create list items for each contact
             data.forEach(contact => {
-              const listItem = document.createElement('li');
-              listItem.innerHTML = `<a href="/chat/${contact.student_id}">${contact.student_id}</a>`;
+              const listItem = document.createElement('div');
+              listItem.classList.add('row', 'col-10')
+              let profileImage = document.createElement('img');
+              profileImage.classList.add('profile-circle-img');
+              profileImage.src = '/images/' + contact.student_id + '.jpg';
+              let column = document.createElement('div');
+              column.classList.add('col-3');
+              column.appendChild(profileImage);
+              col2 = document.createElement('div');
+              col2.innerHTML = contact.student_id;
+              col2.classList.add('col-9');
+              listItem.appendChild(column);
+              listItem.appendChild(col2);
               contactsList.appendChild(listItem);
             });
           })
